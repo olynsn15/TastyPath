48c1a1469b36024ae86aa1d975cb824a5b80ce34
Olyn <carolyn.ardjo@gmail.com>
2025-09-25 16:00:18 +0700

    adding provider

--- src/contexts/MovieContext.tsx
+++ src/contexts/MovieContext.tsx
@@ -1,3 +1,8 @@
 import { createContext, useState, useContext, useEffect } from "react";
 
 const MovieContext = createContext();
+
+export const useMovieContext = () => useContext(MovieContext);
+
+// provide state to any of the components wrapped inside it
+export const MovieProvider = () => {};
